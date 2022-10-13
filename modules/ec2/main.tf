cdfd5fd6a9df36fa7fabc3c3ddd17c497c629e23
sstcheuffa <113473683+sstcheuffa@users.noreply.github.com>
2022-10-12 22:58:36 -0400

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -5,15 +5,15 @@ data "aws_ami" "ec2_instance" {
 
   filter {
     name   = "virtualization-type"
-    values = ["hvm"]
+    values = ["ami-026b57f3c383c2eec", "hvm"]
   }
 
   owners = ["${data.aws_caller_identity.current.account_id}"]
 }
 
 resource "aws_instance" "web" {
-  #ami           = "${data.aws_ami.ec2_instance.id}"
-  ami = "ami-026b57f3c383c2eec"
+  ami           = "${data.aws_ami.ec2_instance.id}"
+  #ami = "ami-026b57f3c383c2eec"
   instance_type = "t2.micro"
 
   tags = {
